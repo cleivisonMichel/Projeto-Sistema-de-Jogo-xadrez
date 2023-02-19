c155b8bdaabe1dd8aa2cdb959245abe3c7eb76c0
ClevisonMichel <cleivisonmicheldev@gmail.com>
2023-02-19 09:58:14 -0300

    Starting to implement Board and Piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,14 +2,15 @@ package application;
 
 import java.util.Scanner;
 
+import boardgame.Board;
 import boardgame.Position;
 
 public class Program {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		Position pos = new Position(3,5);
-		System.out.println(pos);
+	  Board board = new Board(8,8);
+		
 	}
 
 }
